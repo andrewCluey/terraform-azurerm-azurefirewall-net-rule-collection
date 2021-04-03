e5b4f16b786a74ac99a9cb51ced56285bab3a091
andrew.clure@irwinmitchell.com <andrew@andrews-air.home>
2021-04-03 11:40:09 +0100

    added the description input parameter to the rule block

--- main.tf
+++ main.tf
@@ -4,21 +4,35 @@
 resource "azurerm_firewall_network_rule_collection" "fw_net_rule" {
   name                = var.rule_collection_name
   azure_firewall_name = var.azure_fw_name
-  resource_group_name = var.fw_resource_group_name
+  resource_group_name = var.resource_group_name
   priority            = var.rule_priority
   action              = var.rule_action
 
+# At least one of source_addresses and source_ip_groups must be specified for a rule.
+# At least one of destination_addresses, destination_ip_groups and destination_fqdns must be specified for a rule.
   dynamic "rule" {
     for_each = var.rule
     content {
       name                  = rule.value.name                                   # string
-      source_addresses      = rule.value.source_addresses                       # list ["10.0.0.0/16",]
+      description           = lookup(rule.value, "description", null)           # string
+      source_addresses      = lookup(rule.value, "source_addresses", null)      # list EXAMPLE - ["10.0.0.0/16",]
       source_ip_groups      = lookup(rule.value, "source_ip_groups", null)      # list
-      destination_ports     = rule.value.destination_ports                      # list ["53",]
-      destination_fqdns     = lookup(rule.value, "destination_fqdns", null)     # list
+      destination_ports     = rule.value.destination_ports                      # list EXAMPLE - ["53",]
+      destination_fqdns     = lookup(rule.value, "destination_fqdns", null)     # list (DNS Proxy must be enabled to use FQDNs.)
       destination_ip_groups = lookup(rule.value, "destination_ip_groups", null) # list
-      destination_addresses = rule.value.destination_addresses                  # list ["8.8.8.8","8.8.4.4",]
-      protocols             = rule.value.protocols                              # list ["TCP","UDP",]
+      destination_addresses = lookup(rule.value, "destination_addresses", null) # list EXAMPLE -  ["8.8.8.8","8.8.4.4",]
+      protocols             = rule.value.protocols                              # list EXAMPLE - ["TCP","UDP",]
     }
   }
 }
+
+
+
+name - (Required) Specifies the name of the Network Rule Collection which must be unique within the Firewall. Changing this forces a new resource to be created.
+
+azure_firewall_name - 
+
+
+
+
+rule 
--- variables.tf
+++ variables.tf
@@ -1,25 +1,25 @@
 variable "rule_collection_name" {
-  description = "description"
+  description = "(Required) Specifies the name of the Network Rule Collection which must be unique within the Firewall. Changing this forces a new resource to be created."
   type        = string
 }
 
 variable "azure_fw_name" {
-  description = "description"
+  description = "(Required) Specifies the name of the Firewall in which the Network Rule Collection should be created. Changing this forces a new resource to be created."
   type        = string
 }
 
-variable "fw_resource_group_name" {
-  description = "description"
+variable "resource_group_name" {
+  description = "(Required) Specifies the name of the Resource Group in which the Firewall exists. Changing this forces a new resource to be created."
   type        = string
 }
 
 variable "rule_priority" {
-  description = "description"
+  description = "(Required) Specifies the priority of the rule collection. Possible values are between 100 - 65000."
   type        = string
 }
 
 variable "rule_action" {
-  description = "description"
+  description = "(Required) Specifies the action the rule will apply to matching traffic. Possible values are Allow and Deny."
   type        = string
 }
 
